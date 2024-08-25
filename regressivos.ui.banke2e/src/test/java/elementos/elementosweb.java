01e1264bc863ef2a2ffc6797967b7bf90211821f
teste2e <vieiraricael77@gmail.com>
2024-08-25 20:20:08 -0300

    inclusão do projeto

---
+++ regressivos.ui.banke2e/src/test/java/elementos/elementosweb.java
@@ -0,0 +1,11 @@
+package elementos;
+
+import org.openqa.selenium.By;
+
+public class elementosweb {
+
+	By btnlogin = By.xpath("//button{@class='login-button']");
+
+
+
+}
---
+++ src/test/java/elementos/elementosweb.java
@@ -0,0 +1,16 @@
+package elementos;
+
+import org.openqa.selenium.By;
+
+public class elementosweb {
+
+	By btnLogin = By.xpath("//button[@class='login-button']");
+
+	By username = By.xpath("//input[@name='username']");
+	
+	
+	
+	
+	
+	
+}
